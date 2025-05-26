02c62352ebe41a6dec7b395963e6ffafd91e886e
TONY GAO <TONY.GAO@STU.OCSB.CA>
2025-05-26 16:27:43 +0000

    hi

---
+++ five_ints_line.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Created By: Tony G
+
+# Date: 2025-05-26
+
+# 1000-2000
+
+for i in range(1000, 2001):
+    print(i, end=" ")
+    if (i - 999) % 5 == 0:
+        print()
